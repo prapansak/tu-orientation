6daa91d8025b02745da88011756b9b0c73d6524c
Prapansak Kaewlamul <prapansak_kaew@hotmail.com>
2017-07-27 00:12:18 +0700

    fix layout

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -8,33 +8,35 @@ export default class Home extends React.Component {
     render(){
         return(
             <div className='home'>
-                <div className="mdl-grid cover">
+                <div className="mdl-grid">
+                    <div className="mdl-cell mdl-cell--12-col cover">
+                         <img height='100%' src='/assets/cover.png'/> 
+                    </div>
                     <div className="mdl-cell mdl-cell--12-col">
-                         <img className='cover' height='100%' src='/assets/cover.png'/> 
+                        <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+                            <div className="mdl-tabs__tab-bar">
+                                <a href="#home" className="mdl-tabs__tab is-active">หน้าหลัก</a>
+                                <a href="#audit" className="mdl-tabs__tab">ตรวจสอบรายชื่อ</a>
+                                <a href="#qa" className="mdl-tabs__tab">Q&A</a>
+                            </div>
+
+                            <div className="mdl-tabs__panel is-active" id="home">
+                                <MainPanel/>
+                            </div>
+                        
+                            <div className="mdl-tabs__panel" id="audit">
+                                <AuditPanel/>
+                            </div>
+
+                            <div className="mdl-tabs__panel" id="qa">
+                                <QAPanel/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mdl-cell mdl-cell--12-col">
+                        <Footer/> 
                     </div>
                 </div>
-
-                <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-                    <div className="mdl-tabs__tab-bar">
-                        <a href="#home" className="mdl-tabs__tab is-active">หน้าหลัก</a>
-                        <a href="#audit" className="mdl-tabs__tab">ตรวจสอบรายชื่อ</a>
-                        <a href="#qa" className="mdl-tabs__tab">Q&A</a>
-                    </div>
-
-                    <div className="mdl-tabs__panel is-active" id="home">
-                        <MainPanel/>
-                    </div>
-                
-                    <div className="mdl-tabs__panel" id="audit">
-                        <AuditPanel/>
-                    </div>
-
-                    <div className="mdl-tabs__panel" id="qa">
-                        <QAPanel/>
-                    </div>
-                </div>
-
-                <Footer/> 
             </div>
         )
     }
